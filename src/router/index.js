878b4829ce2b9dc22f619153ccf97a8b23131168
Elgolfin <elgo@nicomputer.net>
2018-07-17 19:39:26 -0400

    Integrate with a dummy Prismic repo

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import NotFound from '@/components/NotFound'
 import Preview from '@/components/Preview'
 import Tutorial from '@/components/Tutorial'
+import Page from '@/components/Page'
 
 Vue.use(Router)
 
@@ -27,6 +28,11 @@ const router = new Router({
       path: '/tutorial',
       name: 'tutorial',
       component: Tutorial
+    },
+    {
+      path: '/page/:uid',
+      name: 'page',
+      component: Page
     },
     {
       path: '*',
